8f80b4227704a6bf7865664370721349e01fe589
Ramin <raminshakooei@gmail.com>
2022-07-02 00:52:13 +0430

    summary changed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const headerList = document.querySelector(".header__list");
 const sectionItem = document.querySelectorAll(".section__item");
 const listItem = document.querySelectorAll(".list__item");
+const btnProject = document.querySelector(".btn--project");
 ///
 headerList.addEventListener("click", function (e) {
   const btn = e.target.closest(".list__item");
@@ -14,4 +15,13 @@ headerList.addEventListener("click", function (e) {
   });
   const slide = btn.dataset.name;
   document.querySelector(`.${slide}`).classList.remove("hidden");
+});
+
+btnProject.addEventListener("click", function (e) {
+  listItem.forEach((item) => item.classList.remove("list__item--active"));
+  document
+    .querySelector(".list__item--projects")
+    .classList.add("list__item--active");
+  sectionItem.forEach((section) => section.classList.add("hidden"));
+  document.querySelector(".projects").classList.remove("hidden");
 });
